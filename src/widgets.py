380f19828a7e9a6397ce502e3ada25d51acd828c
Moritz Maier <moritzmaier34@gmail.com>
2025-02-15 12:35:43 +0100

    dark/light mode + colorschemes + about window

--- src/widgets.py
+++ src/widgets.py
@@ -11,12 +11,16 @@ class Widgets(ctk.CTkFrame):
         
         self.font = "Arial"
         self.font_size = 25
+        self.max_font_size = 50
+        self.min_font_size = 25
+        self.text_color = "white"
+        self.fg_color = "#171614"
         self.create_textbox(window)
         self.menu_func(window)
         
         # gedrückte tasten
         self.pressed_keys = set()
-
+        
         window.bind("<KeyPress>",self.key_press)
         window.bind("<KeyRelease>",self.key_release)
         
@@ -30,48 +34,82 @@ class Widgets(ctk.CTkFrame):
 
     def check_combination(self):
        # Reinzoomen 
-       if "Control_L" in self.pressed_keys and "plus" in self.pressed_keys:
+       if "Control_L" in self.pressed_keys and "plus" in self.pressed_keys and self.font_size < self.max_font_size:
            self.font_size += 1
            self.update_textbox_font()
        # Rauszoomen 
-       elif "Control_L" in self.pressed_keys and "minus" in self.pressed_keys:
+       elif "Control_L" in self.pressed_keys and "minus" in self.pressed_keys and self.font_size >= self.min_font_size:
            self.font_size -= 1
            self.update_textbox_font()
     
             
 
     def create_textbox(self,window):
-        self.textbox = ctk.CTkTextbox(window,width=1920,height=1080,font=(self.font,self.font_size))
+        self.textbox = ctk.CTkTextbox(window,width=1920,height=1080,corner_radius=0,text_color=self.text_color,fg_color=self.fg_color,font=(self.font,self.font_size))
         self.textbox.place(x=0,y=0)
 
     def update_textbox_font(self):
-        self.textbox.configure(font=(self.font,self.font_size))
+        self.textbox.configure(fg_color=self.fg_color,text_color=self.text_color,font=(self.font,self.font_size))
 
 
     def menu_func(self,master):
             def open_file():
-                # file path
-                path = filedialog.askopenfile(title="Open File",filetypes=[("Textdateien","*.txt")]).name
+                try:
+                 # file path
+                 path = filedialog.askopenfile(title="Open File",filetypes=[("Textdateien","*.txt",),("Python-Dateien","*.py")],).name
                 
-                if path:
-                    with open(path,"r",encoding="utf-8") as file:
-                        content = file.read()
+                 if path:
+                     with open(path,"r",encoding="utf-8") as file:
+                         content = file.read()
 
-                        self.textbox.delete(1.0,tk.END)
-                        self.textbox.insert(tk.END,content)
-    
-                                
+                         self.textbox.delete(1.0,tk.END)
+                         self.textbox.insert(tk.END,content)
+                except Exception as e:
+                    print("You closed the window. No new file opened.")
                 
             def save_file():
-                path = filedialog.asksaveasfile(title="Save File",filetypes=[("Textdateien","*.txt")]).name
+                try:
+                    path = filedialog.asksaveasfile(title="Save File",filetypes=[("Textdateien","*.txt"),("Python-Dateien","*.py")]).name
                 
-                if path:
-                    with open(path,"w",encoding="utf-8") as file:
-                        content = self.textbox.get(1.0,tk.END) # holt den text aus dem editor
-                        file.write(content) # speichert den text in die datei
+                    if path:
+                        with open(path,"w",encoding="utf-8") as file:
+                            content = self.textbox.get(1.0,tk.END) # holt den text aus dem editor
+                            file.write(content) # speichert den text in die datei
+
+                except Exception as e:
+                    print("You closed the window. No new file saved.")
+
 
             def close_file():
                 sys.exit(0)
+            def change_colors():
+                root = ctk.CTk()
+                root.title("Colorschemes")
+                root.minsize(200,200)
+
+                def change_selected(selected):
+                    if selected == "Hacker colors":
+                        self.text_color = "green"
+                        self.update_textbox_font()
+                    elif selected == "White colors":
+                        self.text_color = "white"
+                        self.update_textbox_font()
+                    elif selected == "Blue colors":
+                        self.text_color = "blue"
+                        self.update_textbox_font()
+                    elif selected == "Red colors":
+                        self.text_color = "red"
+                        self.update_textbox_font()
+
+                colorschemes = CTkListbox(root,width=170,height=180,command=change_selected)
+                colorschemes.place(x=0,y=0)
+
+                colorschemes.insert(0,"Hacker colors")
+                colorschemes.insert(1,"White colors")
+                colorschemes.insert(2,"Red colors")
+                colorschemes.insert(3,"Blue colors")
+
+                root.mainloop()
             
                 
 
@@ -96,8 +134,45 @@ class Widgets(ctk.CTkFrame):
 
                 root.mainloop()
 
+            def about_window():
+                root = ctk.CTk()
+                root.geometry("650x300")
+                root.title("About")
+                root.maxsize(650,300)
+                
+                
+                text_list="""Thanks for using this app!
 
-    
+I'm Currently learning how to make guis with python(customtkinter).
+If you have anything I can do better please make a pull request on my
+github @Moritz344 or if you need any help."""
+
+
+
+
+                normal_text = ctk.CTkTextbox(
+                    master=root,
+                    font=("opensans",20),
+                    width=700,
+                    height=500,
+                    text_color="green",
+                    fg_color="black",
+
+                )
+                normal_text.insert("10.0",text_list)
+                normal_text.configure(state="disabled")
+                normal_text.place(x=0,y=0)
+
+                root.mainloop()
+            def light_mode():
+                self.fg_color = "white"
+                self.text_color = "black"
+                self.update_textbox_font()
+            def dark_mode():
+                self.fg_color = "#171614"
+                self.text_color = "white"
+                self.update_textbox_font()
+
             menu = Menu(master)
             master.config(menu=menu)
     
@@ -107,8 +182,9 @@ class Widgets(ctk.CTkFrame):
             # adding the menus
             menu.add_cascade(label="File", menu=fileMenu)
             menu.add_cascade(label="Help",menu=helpMenu)
+            
 
-            helpMenu.add_command(label="help")
+            helpMenu.add_command(label="About",command=about_window)
             
             
             fileMenu.add_command(label="Open",command=open_file )
@@ -120,6 +196,9 @@ class Widgets(ctk.CTkFrame):
             subMenu = Menu(fileMenu,tearoff=0)
             fileMenu.add_cascade(label="Preferences",menu=subMenu)
             subMenu.add_command(label="Font",command=update_font)
+            subMenu.add_command(label="Colorscheme",command=change_colors)
+            subMenu.add_command(label="Light mode",command=light_mode)
+            subMenu.add_command(label="Dark mode",command=dark_mode)
 
 
 
