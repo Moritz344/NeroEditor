e859f04085d79f0956d077a2efbd03235f89342e
Moritz <157284909+Moritz344@users.noreply.github.com>
2025-04-27 20:32:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,35 @@
 from cx_Freeze import setup, Executable
 import sys
 import os
+import datetime  
 
 today = datetime.date.today()
 version = f"1.0.0.dev{today.strftime('%Y%m%d')}"
 
-include_files = ["assets/","data.json","settings.py","syntax.py","widgets.py","start_screen.py","hand.cur","Normal.cur",     
-"CTkListbox/","CTkMenuBar/","CTkMessagebox/","CTkScrollableDropdown","CTkToolTip/","write_to_json.py","markdown_editor.py"]
+include_files = [
+    "assets/", "data.json", "settings.py", "syntax.py", "widgets.py", "start_screen.py",
+    "hand.cur", "Normal.cur",
+    "CTkListbox/", "CTkMenuBar/", "CTkMessagebox/", "CTkScrollableDropdown", "CTkToolTip/",
+    "write_to_json.py", "markdown_editor.py"
+]
 
 base = None
 if sys.platform == "win32":
-    base = "Win32GUI"  # damit kein Terminal erscheint bei GUI-Apps
+    base = "Win32GUI"  
 
 setup(
     name="NeroEditor",
     version=version,
     description="NeroEditor is a simple and flexible text editor with basic features for editing text.",
-    options={"build_exe": {
-        "packages": ["customtkinter", "tkinter","PIL","sys","json","CTkSpinbox","ctkcomponents"],
-        "include_files": include_files,
-        "include_msvcr": True
-    }},
-    executables=[Executable("main.py", base=base)]
+    options={
+        "build_exe": {
+            "packages": [
+                "customtkinter", "tkinter", "PIL", "sys", "json", "CTkSpinbox", "ctkcomponents"
+            ],
+            "include_files": include_files,
+            "include_msvcr": True
+        }
+    },
+    package_dir={"": "src"},  
+    executables=[Executable("src/main.py", base=base)] 
 )
