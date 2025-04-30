fe5dfb259ef56dd3b1813168f70400c0c302a51c
Moritz Maier <moritzmaier34@gmail.com>
2025-04-30 15:08:42 +0200

    adding things to markdown editor

--- src/markdown_editor.py
+++ src/markdown_editor.py
@@ -6,15 +6,18 @@ from tkinter import filedialog,simpledialog
 from settings import *
 import threading
 from tkinterweb import HtmlFrame
+from tkhtmlview import HTMLLabel
 from CTkMenuBar import *
+import CTkMessagebox
+from ctkcomponents import *
+
 
 class MarkdownViewerApp(ctk.CTkFrame):
     def __init__(self, root):
         super().__init__(root)
         root.geometry("800x600")
         root.title("Markdown Editor")
-
-        # -- 
+        
 
         self.menu = CTkMenuBar(root,)
         file_btn = self.menu.add_cascade("File")
@@ -25,61 +28,107 @@ class MarkdownViewerApp(ctk.CTkFrame):
         dropdown.add_option("New File",command=self.new_file)
         dropdown.add_option("Exit",command=root.destroy)
 
+        self.topbar = ctk.CTkFrame(root,width=1920,height=50,fg_color="black",corner_radius=0)
+        self.topbar.pack()
+
+        bold_btn = ctk.CTkButton(self.topbar,width=10,text="Bold")
+        bold_btn.place(x=10,y=10)
+
+        kursiv_btn = ctk.CTkButton(self.topbar,width=10,text="Kursiv")
+        kursiv_btn.place(x=60,y=10)
+
+        deleted_btn = ctk.CTkButton(self.topbar,width=10,text="//")
+        deleted_btn.place(x=120,y=10)
+
         # Markdown file
         self.textbox_1 = ctk.CTkTextbox(root,width=1000,height=900,border_width=1,
-        border_color="white",corner_radius=0,wrap="word",font=("opensans",standard_font_size))
+        border_color="white",corner_radius=0,wrap="word",tabs=3,font=("opensans",standard_font_size))
         self.textbox_1.pack(side="left",anchor="sw",fill="both")
         # --
 
 
 
-        #convert = ctk.CTkButton(root,text="preview",command=lambda: self.preview(root))
-        #convert.place(x=0,y=0)
-        html = "<br>"
 
         self.path = ""
         
-        # converted box
-        
-        self.html_frame = HtmlFrame(root,width=800,messages_enabled=True)
-        self.html_frame.pack(side="top",fill="both",padx=10,pady=0,expand=True)
-
+        self.html_frame = HtmlFrame(root,width=800,zoom=1)
+        self.html_frame.pack(side="top",fill="both",padx=10,pady=00,expand=True)
+            
         self.textbox_1.bind("<KeyPress>",lambda event: self.preview(root,event))
         self.textbox_1.bind("<KeyRelease>",lambda event: self.preview(root,event))
 
-    def new_file(self,window):
-        file_path = filedialog.askcreatefile()
+
+
+    def new_file(self,):
+        file_path = filedialog.asksaveasfilename(title="Create File",filetypes=[("Markdown","*.md")])
+        
+        try:
+            with open(file_path,"w",encoding="utf-8") as file:
+                content = self.textbox_1.get("0.0","end")
+                file.write(content)
+        except Exception as e:
+            print(e)
+
     def open_file(self,):
         file_path = filedialog.askopenfile(title="Open Markdown File",
-        filetypes=[("Markdown","*.md")]
+        filetypes=[("Show Markdown files","*.md",),("Show all files","*.*")]
         ).name
 
         self.path = file_path
+        
+        try:
+            with open(file_path,"r",encoding="utf-8") as file:
+                content = file.read()
 
-        with open(file_path,"r",encoding="utf-8") as file:
-            content = file.read()
-
-            self.textbox_1.delete("0.0","end")
-            self.textbox_1.insert("end",content)
+                self.textbox_1.delete("0.0","end")
+                self.textbox_1.insert("end",content)
+        except Exception as e:
+            print(e)
 
 
     def save_file(self,):
-        with open(self.path,"w+") as file:
-            content = self.textbox_1.get("0.0","end")
-            file.write(content)
+        if self.path != "":
+            with open(self.path,"w+") as file:
+                content = self.textbox_1.get("0.0","end")
+                file.write(content)
+        else:
+            self.new_file()
 
+    def wrap_html_with_style(self,html_content: str) -> str:
+        return f"""
+        <html>
+          <head>
+            <style>
+              body {{
+                font-family: 'Arial', sans-serif;
+                font-size: 16px;
+                padding: 20px;
+                background-color: #f0f0f0;
+              }}
+              h1 {{
+                color: darkblue;
+              }}
+              code {{
+                color: red;
+              }}
+            </style>
+          </head>
+          <body>
+            {html_content}
+          </body>
+        </html>
+        """
 
-        
     
     def preview(self,window,event=None):
-        text_input = self.textbox_1.get("0.0","end-1c")
-        full_text = text_input.replace("\n\n", "\n<br>\n")
+        text_input = self.textbox_1.get("0.0","end")
+        full_input = text_input.replace("\n","<br>")
         
-        html = markdown.markdown(full_text)
-        
-        self.html_frame.load_html(html)
+        html_1 = markdown.markdown(text_input)
+        html_output = self.wrap_html_with_style(html_1)
+        self.html_frame.load_html(html_output)
 
 
-#root = ctk.CTk()
-#MarkdownViewerApp(root)
-#root.mainloop()
+root = ctk.CTk()
+MarkdownViewerApp(root)
+root.mainloop()
