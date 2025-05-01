9a0ef16ef914c89bcd532ae8386ca867d4718388
Moritz344 <moritzmaier34@gmail.com>
2025-05-01 19:20:04 +0200

    fast btns + fixed bug with newlines

--- src/markdown_editor.py
+++ src/markdown_editor.py
@@ -1,16 +1,14 @@
 import markdown
-import webview
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import filedialog,simpledialog
 from settings import *
 import threading
 from tkinterweb import HtmlFrame
-from tkhtmlview import HTMLLabel
 from CTkMenuBar import *
 import CTkMessagebox
-from ctkcomponents import *
 
+ctk.set_appearance_mode("dark")
 
 class MarkdownViewerApp(ctk.CTkFrame):
     def __init__(self, root):
@@ -31,14 +29,17 @@ class MarkdownViewerApp(ctk.CTkFrame):
         self.topbar = ctk.CTkFrame(root,width=1920,height=50,fg_color="black",corner_radius=0)
         self.topbar.pack()
 
-        bold_btn = ctk.CTkButton(self.topbar,width=10,text="Bold")
+        bold_btn = ctk.CTkButton(self.topbar,width=10,text="Bold",command=self.bold_btn)
         bold_btn.place(x=10,y=10)
 
-        kursiv_btn = ctk.CTkButton(self.topbar,width=10,text="Kursiv")
+        kursiv_btn = ctk.CTkButton(self.topbar,width=10,text="Kursiv",command=self.kursiv_btn)
         kursiv_btn.place(x=60,y=10)
 
-        deleted_btn = ctk.CTkButton(self.topbar,width=10,text="//")
-        deleted_btn.place(x=120,y=10)
+        list_btn = ctk.CTkButton(self.topbar,width=10,text="List",command=self.list_item)
+        list_btn.place(x=120,y=10)
+
+        header_btn = ctk.CTkButton(self.topbar,width=10,text="T",command=self.header_btn)
+        header_btn.place(x=170,y=10)
 
         # Markdown file
         self.textbox_1 = ctk.CTkTextbox(root,width=1000,height=900,border_width=1,
@@ -57,17 +58,38 @@ class MarkdownViewerApp(ctk.CTkFrame):
         self.textbox_1.bind("<KeyPress>",lambda event: self.preview(root,event))
         self.textbox_1.bind("<KeyRelease>",lambda event: self.preview(root,event))
 
+    def list_item(self):
+        pos = self.textbox_1.index("insert")
+        self.textbox_1.insert(pos,"- List item")
 
+    def header_btn(self):
+        pos = self.textbox_1.index("insert")
+        self.textbox_1.insert(pos,"# Headline")
+
+
+    def kursiv_btn(self):
+        pos = self.textbox_1.index("insert")
+        self.textbox_1.insert(pos,"**")
+
+        self.textbox_1.mark_set("insert",f"{pos} +1c ")
+    def bold_btn(self):
+        pos = self.textbox_1.index("insert")
+        self.textbox_1.insert(pos,"****")
+
+        # 3 stellen nach vorne bewegen
+        self.textbox_1.mark_set("insert",f"{pos} +2c ")
 
     def new_file(self,):
-        file_path = filedialog.asksaveasfilename(title="Create File",filetypes=[("Markdown","*.md")])
+        file_path = filedialog.asksaveasfilename(title="Create File",
+        filetypes=[("Markdown","*.md")])
         
-        try:
-            with open(file_path,"w",encoding="utf-8") as file:
-                content = self.textbox_1.get("0.0","end")
-                file.write(content)
-        except Exception as e:
-            print(e)
+        if file_path:
+          try:
+              with open(file_path,"w",encoding="utf-8") as file:
+                  content = self.textbox_1.get("0.0","end")
+                  file.write(content)
+          except Exception as e:
+              print(e)
 
     def open_file(self,):
         file_path = filedialog.askopenfile(title="Open Markdown File",
@@ -76,14 +98,15 @@ class MarkdownViewerApp(ctk.CTkFrame):
 
         self.path = file_path
         
-        try:
-            with open(file_path,"r",encoding="utf-8") as file:
-                content = file.read()
+        if file_path:
+          try:
+              with open(file_path,"r",encoding="utf-8") as file:
+                  content = file.read()
 
-                self.textbox_1.delete("0.0","end")
-                self.textbox_1.insert("end",content)
-        except Exception as e:
-            print(e)
+                  self.textbox_1.delete("0.0","end")
+                  self.textbox_1.insert("end",content)
+          except Exception as e:
+              print(e)
 
 
     def save_file(self,):
@@ -106,10 +129,19 @@ class MarkdownViewerApp(ctk.CTkFrame):
                 background-color: #f0f0f0;
               }}
               h1 {{
-                color: darkblue;
+                color: dark blue;
+              }}
+              h2 {{
+                color: #274c77;
+              }}
+              h3{{
+                color: #274c77;
+              }}
+              h4 {{
+                color: #274c77;
               }}
               code {{
-                color: red;
+                color: #f9e2af;
               }}
             </style>
           </head>
@@ -124,7 +156,7 @@ class MarkdownViewerApp(ctk.CTkFrame):
         text_input = self.textbox_1.get("0.0","end")
         full_input = text_input.replace("\n","<br>")
         
-        html_1 = markdown.markdown(text_input)
+        html_1 = markdown.markdown(text_input, extensions=["nl2br"]) # extension ding rendert zeilenumbrüche in <br> um danke!!!
         html_output = self.wrap_html_with_style(html_1)
         self.html_frame.load_html(html_output)
 
